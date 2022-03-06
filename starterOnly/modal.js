1eb59696ed2cae31947c5255566d8a6ff2986b1c
clemgbld <clementgombauld@hotmail.fr>
2022-03-06 12:43:12 +0100

    validation for firstname and lastname

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,11 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelector(".close");
+const formEl = document.querySelector(".form");
+const firstInputEl = document.getElementById("first");
+const errorFirstEl = document.querySelector(".first-error");
+const lastInputEl = document.getElementById("last");
+const errorLastEl = document.querySelector(".last-error");
 
 // launch modal form
 const launchModal = () => {
@@ -28,3 +33,43 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // close modal event
 modalCloseBtn.addEventListener("click", closeModal);
+
+// from validation
+
+//// show error function
+const showError = (errorEl) => {
+  errorEl.style.display = "block";
+};
+
+//// hide error function
+const hideError = (errorEl) => {
+  errorEl.style.display = "none";
+};
+
+// validation conditions
+
+const formValidation = (e) => {
+  // prevent the form from submitting
+  e.preventDefault();
+
+  // initialize the number of error
+  let numberOfError = 0;
+
+  //// Check the validation of the input or the box
+
+  const checkValidation = (condition, errorEl) => {
+    if (!condition) {
+      numberOfError++;
+      return showError(errorEl);
+    }
+
+    hideError(errorEl);
+  };
+
+  // check if first name has at least 2 charactères
+  checkValidation(firstInputEl.value.trim().length >= 2, errorFirstEl);
+  // check if first name has at least 2 charactères
+  checkValidation(lastInputEl.value.trim().length >= 2, errorLastEl);
+};
+
+formEl.addEventListener("submit", formValidation);
