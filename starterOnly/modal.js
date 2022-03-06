8db8cb75edb73e3ec44e4fc34829b05dbff9f631
clemgbld <clementgombauld@hotmail.fr>
2022-03-06 11:11:53 +0100

    close modal done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,30 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+const editNav = () => {
+  let navEl = document.getElementById("myTopnav");
+  if (navEl.className === "topnav") {
+    navEl.className += " responsive";
   } else {
-    x.className = "topnav";
+    navEl.className = "topnav";
   }
-}
+};
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close");
+
+// launch modal form
+const launchModal = () => {
+  modalbg.style.display = "block";
+};
+
+// close modal form
+const closeModal = () => {
+  modalbg.style.display = "none";
+};
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
-
+// close modal event
+modalCloseBtn.addEventListener("click", closeModal);
